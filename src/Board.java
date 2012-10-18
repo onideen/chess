db8d83d724b0059e57188785ae72e1f67325a25a
Vegar Engen <vegar.engen@akuma>
2012-10-18 16:38:48 -0700

    Board is initiated and writeable

--- src/Board.java
+++ src/Board.java
@@ -11,7 +11,25 @@ public class Board {
 	
 	private void initBoard() {
 		board = new Piece[BOARD_SIZE][BOARD_SIZE];
+		arrangePieces(Color.WHITE);
+		arrangePieces(Color.BLACK);
+	}
+
+	private void arrangePieces(Color color) {
+		int mainY = (color == Color.WHITE ? 0 : 7);
+		int pawnY = (color == Color.WHITE ? 1 : 6);
+		board[0][mainY] = new Rook(this, color);
+		board[1][mainY] = new Knight(this, color);
+		board[2][mainY] = new Bishop(this, color);
+		board[3][mainY] = new Queen(this, color);
+		board[4][mainY] = new King(this, color);
+		board[5][mainY] = new Bishop(this, color);
+		board[6][mainY] = new Knight(this, color);
+		board[7][mainY] = new Rook(this, color);
 		
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			board[i][pawnY] = new Pawn(this, color);
+		}		
 	}
 
 	public boolean isEmpty(int x, int y){
@@ -34,15 +52,15 @@ public class Board {
 		StringBuilder board = new StringBuilder();
 		
 		for (int y = BOARD_SIZE-1; y >= 0; y--) {
-			board.append("+---------------------------------------+\n");
+			board.append("+-------------------------------+\n");
 			board.append("| ");
 			for (int x = 0; x < BOARD_SIZE; x++) {
-				board.append(this.board[x][y] == null ? "  " : this.board[x][y]);
+				board.append(this.board[x][y] == null ? " " : this.board[x][y]);
 				board.append(" | ");
 			}
 			board.append("\n");
 		}
-		board.append("+---------------------------------------+\n");
+		board.append("+-------------------------------+\n");
 		
 		return board.toString();
 	}
