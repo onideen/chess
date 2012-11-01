930a72ce70c4cab608a8e63f0386033d40c671e5
gjoranm <gjoranm@gjoranmLappis>
2012-11-02 00:34:41 +0100

    canTake & canMove fixed

--- src/Pawn.java
+++ src/Pawn.java
@@ -6,11 +6,24 @@ public class Pawn extends AbstractPiece {
 	
 	@Override
 	public boolean canMove(String from, String to, Board board) {
-		if (this.getPieceColor()==PieceColor.WHITE) {
-			return Board.getRowDistance(from, to)==1 && Board.getColumnDistance(from, to)==0;
+		int rd =Board.getRowDistance(from, to);
+		int cd =Board.getColumnDistance(from, to);
+		if (this.getPieceColor()==PieceColor.WHITE) {//White player
+			if (Board.getRowIndex(from)==1) {
+				return rd <= 2 && cd == 0 && rd>0;
+			}
+			else {
+				return rd == 1 && cd == 0;
+			}
 		}
-		else {
-			return Board.getRowDistance(from, to)==-1 && Board.getColumnDistance(from, to)==0;
+		else {//Black player
+			if (Board.getRowIndex(from)==6) {
+
+				return rd >= -2 && cd== 0 && rd<0;
+			}
+			else {
+				return rd == -11 && cd == 0;
+			}
 		}
 	}
 	
