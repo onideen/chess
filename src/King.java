e3d5e6a0ec70363c1628a5146eb361933f731ead
gjoranm <gjoranmo@gmail.com>
2012-11-02 00:06:40 +0100

    Update src/King.java

--- src/King.java
+++ src/King.java
@@ -1,4 +1,17 @@
+import java.math.*;
 
-public class King {
+public class King extends AbstractPiece {
+
+  public King(PieceColor color){
+		super(color);
+	}
+	
+	
+	@Override
+	public boolean canMove(String from, String to, Board board) {
+		return (Board.isDiagonal(from, to) || Board.isStraight(from, to))
+		&&(Math.abs(Board.getColumnDistance(from, to))<=1
+				&&Math.abs(Board.getRowDistance(from, to))<=1 );
+	}
 
 }
